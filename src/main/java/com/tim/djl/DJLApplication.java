af2790b85356f6599ebe1610d247729ef084afd2
jgemanimtim <jgemanimtim@gmail.com>
2025-09-29 12:37:47 +0800

    Initial Scaffolding

---
+++ src/main/java/com/tim/djl/DJLApplication.java
@@ -0,0 +1,13 @@
+package com.tim.djl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DJLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DJLApplication.class, args);
+	}
+
+}
